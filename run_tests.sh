aeb48b4041cf0b8bb85e0e95672949ba97db27fc
Patryk Zdral <patrykz13@gmail.com>
2018-06-11 01:13:20 +0200

    changed script.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,8 @@ set -e
 virtualenv venv --distribute -p python3
 source venv/bin/activate
 pip install -r requirements.txt
-PYTHONPATH=. venv/bin/python3 tests/calculations_tests.py
-PYTHONPATH=. venv/bin/coverage run tests/calculations_tests.py
+PYTHONPATH=. venv/bin/python3 testing/test_simple.py
+PYTHONPATH=. venv/bin/coverage run testing/test_simple.py
 PYTHONPATH=. venv/bin/coverage report --omit=venv/*
 PYTHONPATH=. venv/bin/coverage html --omit=venv/*
 PYTHONPATH=. venv/bin/coverage xml --omit=venv/*
