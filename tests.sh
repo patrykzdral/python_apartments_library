ec965c6eeda946b183e35ce630dcff9aa84bd190
Patryk Zdral <patrykz13@gmail.com>
2018-06-11 14:21:05 +0200

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -5,4 +5,5 @@ virtualenv venv --distribute -p python3
 source venv/bin/activate
 pip install -r requirements.txt
 PYTHONPATH=. venv/bin/coverage run testing/test_simple.py
+PYTHONPATH=. venv/bin/python3 testing/test_simple.py
 PYTHONPATH=. venv/bin/coverage xml --omit=venv/*
